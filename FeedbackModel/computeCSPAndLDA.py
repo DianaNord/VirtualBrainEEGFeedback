d576744bb45c13548b9f1c9054d5b62e304dda08
Diana <diana.nordtvedt@student.tugraz.at>
2022-03-20 18:00:47 +0100

    optimized workflow

--- FeedbackModel/computeCSPAndLDA.py
+++ FeedbackModel/computeCSPAndLDA.py
@@ -4,43 +4,31 @@
 import json
 import os
 import scipy.io
-import fnmatch
-import numpy as np
 
 from bci_modules import CSPAndLDA
 
 if __name__ == "__main__":
-    directory = 'data/recordings/current/mat-files/'
-    directory_out = directory + '../'
-    config_file_name = '/../bci-config.json'
+
+    modality = 'ME'
+    subject_id = 'sub-P001'
 
     cwd = os.getcwd()
+    root_dir = cwd + '/data/current/'
+    dir_in = root_dir + subject_id + '/' + modality + '/'
+    dir_out = dir_in + '../'
+    file_path = dir_in + 'messung.mat'
+
+    config_file_name = '/../bci-config.json'
     config_file = cwd + config_file_name
 
     # load bci configuration
     with open(config_file) as json_file:
         config = json.load(json_file)
 
-    eeg = []
-    labels = []
-    file_list = []
-    for path, folders, files in os.walk(directory):
-        for file in files:
-            if fnmatch.fnmatch(file, '*.mat'):
-                file_list.append(os.path.join(path, file))
-
-    first_file = True
-    for file in file_list:
-        data_mat = scipy.io.loadmat(file)['data'].T
-        if first_file:
-            labels = data_mat[0, :]
-            eeg = data_mat[1:, :]
-        else:
-            labels = np.append(labels, data_mat[0, :])
-            eeg = np.append(eeg, data_mat[1:, :], axis=1)
-
-        first_file = False
+    data_mat = scipy.io.loadmat(file_path)['data'].T
+    labels = data_mat[0, :]
+    eeg = data_mat[1:, :]
 
     csp_lda_obj = CSPAndLDA(eeg=eeg, labels=labels, bci_config=config)
-    csp_lda_obj.compute_csp(output_path=directory_out)
-    csp_lda_obj.compute_lda(output_path=directory_out)
+    csp_lda_obj.compute_csp(output_path=dir_out)
+    csp_lda_obj.compute_lda(output_path=dir_out)
--- FeedbackModel/xdfToMat.py
+++ FeedbackModel/xdfToMat.py
@@ -5,8 +5,8 @@ import pyxdf
 import scipy.io
 
 
-def load_xdf(file_name, lsl_config):
-    streams, fileheader = pyxdf.load_xdf(file_name)
+def load_xdf():
+    streams, fileheader = pyxdf.load_xdf(xdf_file_path)
     streams_info = []
 
     for stream in streams:
@@ -14,14 +14,21 @@ def load_xdf(file_name, lsl_config):
 
     streams_info = np.array(streams_info)
 
+    lsl_config = config['general-settings']['lsl-streams']
+
     eeg_pos = np.where(streams_info == lsl_config['eeg']['name'])[0][0]
     marker_pos = np.where(streams_info == lsl_config['marker']['name'])[0][0]
 
-    return streams[eeg_pos], streams[marker_pos]
+    if mode == 'ONLINE':
+        return streams[eeg_pos], streams[marker_pos]
+    elif mode == 'OFFLINE':
+        erds_pos = np.where(streams_info == lsl_config['fb-erds']['name'])[0][0]
+        lda_pos = np.where(streams_info == lsl_config['fb-lda']['name'])[0][0]
+        return streams[eeg_pos], streams[marker_pos], streams[erds_pos], streams[lda_pos]
 
 
-def add_class_labels_to_eeg(stream_eeg, stream_marker):
-    eeg_time = stream_eeg['time_stamps']
+def add_class_labels(stream):
+    time = stream['time_stamps']
     marker_series = np.array(stream_marker['time_series'])
     cue_times = (stream_marker['time_stamps'])[np.where(marker_series == 'Cue')[0]]
 
@@ -29,12 +36,12 @@ def add_class_labels_to_eeg(stream_eeg, stream_marker):
     conditions[np.where(conditions == 'Start_of_Trial_l')] = 121
     conditions[np.where(conditions == 'Start_of_Trial_r')] = 122
 
-    cue_positions = np.zeros((np.shape(eeg_time)[0], 1), dtype=int)
+    cue_positions = np.zeros((np.shape(time)[0], 1), dtype=int)
     for t, c in zip(cue_times, conditions):
-        pos = find_nearest_index(eeg_time, t)
+        pos = find_nearest_index(time, t)
         cue_positions[pos] = c
 
-    return np.append(cue_positions, stream_eeg['time_series'], axis=1)
+    return np.append(cue_positions, stream['time_series'], axis=1)
 
 
 def find_nearest_index(array, value):
@@ -45,26 +52,68 @@ def find_nearest_index(array, value):
         return idx
 
 
-def save_to_mat(file_path, data):
+def save_to_mat(file_path, identifier, data):
     if os.path.isfile(file_path):
-        data_old = scipy.io.loadmat(mat_file_path)['data']
+        data_old = scipy.io.loadmat(file_path)[identifier]
         data = np.append(data_old, data, axis=0)
 
-    scipy.io.savemat(mat_file_path, {'data': data})
+    scipy.io.savemat(file_path, {identifier: data})
 
 
 if __name__ == "__main__":
-    directory = 'data/recordings/current/ses-S004/eeg/'
-    xdf_file_path = directory + 'sub-P001_ses-S004_task-Default_run-001_eeg.xdf'
-    mat_file_path = directory + 'messung.mat'
+
+    mode = 'ONLINE'
+
+    modality = 'ME'
+    subject_id = 'sub-P001'
+    session = 'ses-S002'
+    run = 'run-001'
 
     cwd = os.getcwd()
-    config_file = cwd + '/../bci-config.json'
 
-    # Read BCI Configuration
-    with open(config_file) as json_file:
-        config = json.load(json_file)
+    if mode == 'ONLINE':
+        root_dir = cwd + '/data/current/'
+        out_dir = root_dir + subject_id + '/' + modality
 
-    eeg_stream, marker_stream = load_xdf(xdf_file_path, config['general-settings']['lsl-streams'])
-    eeg_and_label = add_class_labels_to_eeg(eeg_stream, marker_stream)
-    save_to_mat(mat_file_path, eeg_and_label)
+        file_name = subject_id + '_' + session + '_task-Default_' + run + '_eeg.xdf'
+        xdf_file_path = root_dir + subject_id + '/' + session + '/eeg/' + file_name
+        mat_file_path = out_dir + '/messung.mat'
+
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        # Read BCI Configuration
+        config_file = cwd + '/../bci-config.json'
+        with open(config_file) as json_file:
+            config = json.load(json_file)
+
+        # Read XDF and save as mat
+        stream_eeg, stream_marker = load_xdf()
+        eeg_and_label = add_class_labels(stream_eeg)
+        save_to_mat(mat_file_path, 'data', eeg_and_label)
+
+    elif mode == 'OFFLINE':
+
+        root_dir = cwd + '/../../pilot-study/'
+        out_dir = root_dir + subject_id + '/' + session + '/' + modality
+
+        file_name = subject_id + '_' + session + '_task-Default_' + run + '_eeg.xdf'
+        xdf_file_path = root_dir + subject_id + '/' + session + '/eeg/' + file_name
+
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        config_file = root_dir + subject_id + '/bci-config.json'
+        with open(config_file) as json_file:
+            config = json.load(json_file)
+
+        # Read XDF and save as mat
+        stream_eeg, stream_marker, stream_erds, stream_lda = load_xdf()
+
+        eeg_with_labels = add_class_labels(stream_eeg)
+        erds_with_labels = add_class_labels(stream_erds)
+        lda_with_labels = add_class_labels(stream_lda)
+
+        save_to_mat(out_dir+'/eeg.mat', 'eeg',  eeg_with_labels)
+        save_to_mat(out_dir+'/erds.mat', 'erds', erds_with_labels)
+        save_to_mat(out_dir+'/lda.mat', 'lda', lda_with_labels)
